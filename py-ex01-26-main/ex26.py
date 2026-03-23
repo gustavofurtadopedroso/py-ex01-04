ee4a8c6bc3ca99bf805a2ffe1460090887ff4fb6
Gaucho Gustavo Furtado <150378625+gustavofurtadopedroso@users.noreply.github.com>
2026-03-23 20:22:26 -0300

    Add files via upload

---
+++ py-ex01-26-main/ex26.py
@@ -0,0 +1,17 @@
+print ("1 - Hámburguer - R$ 15.00")
+print ("2 - Pizza - R$ 20.00")
+print(" 3 - Refrigerante - R$ 8.00")
+
+opcao = int(input("Escolha uma opção: "))
+
+if opcao == 1:
+    print("Você escolheu Hámburguer.")
+    print("Valor: R$ 15.00")
+elif opcao == 2:
+    print("Você escolheu Pizza.")
+    print("Valor: R$ 20.00")
+elif opcao == 3:
+    print("Você escolheu Refrigerante.")
+    print("Valor: R$ 8.00")
+else:
+    print("Opção inválida. Por favor, escolha uma opção válida.")
